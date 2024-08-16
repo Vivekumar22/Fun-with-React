f3a396ce05f0205cfb95a63f9b4c5e96491119d1
Vivekumar22 <111337482+Vivekumar22@users.noreply.github.com>
2024-08-16 13:16:39 +0530

    commit

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-      
+      <Header />
+        <Outlet />
+      <Footer />
 
     </>
   )
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
 import Github, {githubloaderinfo} from './components/Github/Github.jsx'
+import App from './App.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -32,7 +31,7 @@ import Github, {githubloaderinfo} from './components/Github/Github.jsx'
 // ])
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<Layout />}>
+  <Route path='/' element={<App />}>
     <Route path="" element={<Home />}/>
     <Route path="about" element={<About />}/>
     <Route path="contact" element={<Contact />}/>
