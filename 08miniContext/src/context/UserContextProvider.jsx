c9e4231f4c2ff9706b1732dd00d0db529cab5221
Vivekumar22 <111337482+Vivekumar22@users.noreply.github.com>
2024-08-16 16:49:33 +0530

    contextApi

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -4,10 +4,12 @@ import UserContext from "./userContext";
 const UserContextProvider = ({children}) => {
     const [user, setUser] = React.useState(null)
     return(
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider value={{user, setUser}}> 
         {children}
         </UserContext.Provider>
     )
 }
 
 export default UserContextProvider
+
+// we pass children it show wrapping whatever we going to wrap  other component like Login,profile
